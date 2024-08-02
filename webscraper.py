a3ee8cd7e7d2596543481d671415f20894b30fdd
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-08-01 18:58:01 -0700

    Updated web scraping

--- webscraper.py
+++ webscraper.py
@@ -1,22 +1,25 @@
 import requests
 import json
 
+LOCATION = '34.052235,-118.243683'  # Latitude and Longitude for Los Angeles
+RADIUS = 5000  # Search within 5 km radius
+LOCATION_TYPE = 'restaurant'
 
-# Load API key from config.json
-with open('config.json', 'r') as file:
-    config = json.load(file)
-api_key = config['PLACES_API_KEY']
+def get_google_places(location, radius, location_type, api_key):
+    place_search_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={location}&radius={radius}&type={location_type}&key={api_key}"
+
+    response = requests.get(place_search_url)
+    restaurants = response.json()
+
+    return restaurants
 
 
-location = '34.052235,-118.243683'  # Latitude and Longitude for Los Angeles
-radius = 5000  # Search within 5 km radius
-location_type = 'restaurant'
+if __name__ == "__main__":
+    
+    # Load API key from config.json
+    with open('config.json', 'r') as file:
+        config = json.load(file)
+    api_key = config['PLACES_API_KEY']
 
-place_search_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={location}&radius={radius}&type={location_type}&key={api_key}"
-
-response = requests.get(place_search_url)
-restaurants = response.json().get('results', [])
-
-print(restaurants)
-
-# place_details_url = "https://maps.googleapis.com/maps/api/place/details/json"
+    restaurant_json = get_google_places(LOCATION, RADIUS, LOCATION_TYPE, api_key)
+    print(restaurant_json['results'][0].keys())
