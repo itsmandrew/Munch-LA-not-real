e231d28551074d2953975fe4c5aa04595da7996f
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-08-03 21:26:39 -0700

    Client setup for OpenAI

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
 import json
-from sentence_transformers import SentenceTransformer
+from openai import OpenAI
+
+with open('config.json', 'r') as file:
+    config = json.load(file)
+    OPEN_AI_ORG_ID = config['OPEN_AI_ORG_ID']
+    OPEN_AI_PROJECT_ID = config['OPEN_AI_PROJECT_ID']
+    OPEN_AI_API_KEY = config['OPEN_AI_API_KEY']
+
+client = OpenAI(
+  organization=OPEN_AI_ORG_ID,
+  project=OPEN_AI_PROJECT_ID,
+  api_key=OPEN_AI_API_KEY
+)
+
