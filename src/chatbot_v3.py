05dba5c31b9a56ad19f01c6790f435c469478550
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-08-15 19:05:49 -0700

    Linted chatbot_v3.py

--- src/chatbot_v3.py
+++ src/chatbot_v3.py
@@ -1,16 +1,24 @@
+# pylint: disable=E0401
+# pylint: disable=W0718
+
+"""
+This module implements a chatbot using LangChain tools and OpenAI's GPT model.
+It initializes a retriever tool and a TavilySearchResults instance, constructs a ReAct agent,
+and provides a loop for user interaction.
+"""
+
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
-from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from utils.helpers import get_openai_key, chromadb_init, get_tavily_key
-from langchain.memory import ConversationBufferMemory
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain.tools.retriever import create_retriever_tool
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain import hub
-from langchain_core.messages import AIMessage, HumanMessage
+from utils.helpers import get_openai_key, chromadb_init
 
 def main():
+    """
+    Main function to run the chatbot application. It initializes necessary tools,
+    constructs the ReAct agent, and enters a loop to handle user input.
+    """
     api_key = get_openai_key('src/config.json')
     langchain_chroma = chromadb_init('src/config.json')
 
@@ -20,9 +28,10 @@ def main():
     )
 
     retriever_tool = create_retriever_tool(
-    retriever,
+        retriever,
         "reviews_search",
-        "Search for information about Restaurant reviews. Use this tool for basic restaurant search.",
+        "Search for information about Restaurant reviews. "
+        "Use this tool for basic restaurant search."
     )
 
     tools = [search, retriever_tool]
@@ -39,18 +48,22 @@ def main():
     # Main loop for user interaction
     while True:
         user_input = input("Enter your question (or 'exit' to quit): ")
-        
+
         if user_input.lower() == 'exit':
             print("Goodbye!")
             break
-        
+
         try:
             response = agent_executor.invoke({"input": user_input})
             print("\nAgent's response:")
             print(response['output'])
+        except (ValueError, TypeError) as e:
+            print(f"An error occurred with the input or response processing: {str(e)}")
+        except RuntimeError as e:
+            print(f"An error occurred with the runtime execution: {str(e)}")
         except Exception as e:
-            print(f"An error occurred: {str(e)}")
-        
+            print(f"An unexpected error occurred: {str(e)}")
+
         print("\n" + "-"*50 + "\n")  # Separator for readability
 
 if __name__ == "__main__":
