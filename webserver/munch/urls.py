ff7f8cf5a47c471d27d855ceb6e7d531a9908c98
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-08-17 16:24:36 -0700

    Deleting webserver/munch

--- webserver/munch/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import langchain_response, home
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('langchain-response/', langchain_response, name='langchain_response'),
-]
