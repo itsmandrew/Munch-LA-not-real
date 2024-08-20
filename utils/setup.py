c778ff4f8b209e7979190669a1ed89e1168deec6
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-08-19 22:41:22 -0700

    Utils moved, gitignore updated for .env, added load_env.sh script

---
+++ utils/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helpers',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A description of your package',
+    author='Munch',
+    url='https://github.com/itsmandrew/Munch-LA',  # Optional
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
